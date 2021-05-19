7ef65b4319b4865cc599e5af53b75f8cf95bd1d2
Camil88 <57962280+Camil88@users.noreply.github.com>
2021-05-19 19:41:52 +0200

    Update utils_helpers.R

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -1,14 +1,11 @@
 
 # points tooltip
-
-createLabel <- function(data) {
-  
+createLabel <- function(data) {  
   labelText <- paste0("<b style='color:#00a2ff'>Driver: </b>",data$driverName,"<br/>",
                       "<b style='color:#00a2ff'>Client: </b>",data$customerName,"<br/>",
                       "<b style='color:#00a2ff'>Transit: </b>",data$transitNr,"<br/>",
                       "<b style='color:#00a2ff'>Order: </b>",data$orderNr,"<br/>",
-                      "<b style='color:#00a2ff'>Status time: </b>",data$statusDateTime,"<br/>")
-  
+                      "<b style='color:#00a2ff'>Status time: </b>",data$statusDateTime,"<br/>")  
 }
 
 
