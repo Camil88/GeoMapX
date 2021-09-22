7b9fed3ce67e39c7bfee022586375752d3022bd9
Camil88 <57962280+Camil88@users.noreply.github.com>
2021-09-22 13:26:18 +0200

    Update fct_mapTokenAndShapes.R

--- R/fct_mapTokenAndShapes.R
+++ R/fct_mapTokenAndShapes.R
@@ -1,6 +1,6 @@
 
 # MAPBOX token and other resources ------------------------------------------------------------
 
-options(mapbox.accessToken = "YOUR_TOKEN")
+options(mapbox.accessToken = "YOUR_TOKEN") # add your Mapbox token
 districts <- sf::st_read(dsn = "https://raw.githubusercontent.com/ppatrzyk/polska-geojson/master/wojewodztwa/wojewodztwa-medium.geojson")
 rsconnect::setAccountInfo(name='kghwl', token='BC3B81C5BF29A03FBDAB6698312706C2', secret='wf6w4piUPN1hCE1D3cKNPHxnEOhLTcLFlqBXXxt2')
